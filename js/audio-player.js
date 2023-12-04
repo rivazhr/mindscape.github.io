8260d2a61e7774e52c5da40ae8d7ef632b496075
rivazhr <rifafairuzzahra@gmail.com>
2023-12-04 11:02:23 +0700

    audio-player-js

--- js/audio-player.js
+++ js/audio-player.js
@@ -1,27 +1,27 @@
-// audioPlayer.js
+document.addEventListener('DOMContentLoaded', function () {
+    const playButtons = document.querySelectorAll('.play-btn');
 
-(function () {
-  var playButtons = document.querySelectorAll('.play-btn');
+    playButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const audioId = button.getAttribute('data-audio-id');
+            const audio = document.getElementById(audioId);
+            const progressBar = document.querySelector(`.progress-bar-${audioId}`);
 
-  playButtons.forEach(button => {
-      button.addEventListener('click', () => {
-          var audioId = button.getAttribute('data-audio-id');
-          var audio = document.getElementById(audioId);
-          var progressBar = document.querySelector(`.progress-bar-${audioId}`);
+            if (audio.paused) {
+                audio.play().then(() => {
+                    button.innerHTML = '<img src="/images/icons/Pause.svg" alt="">';
+                }).catch(error => {
+                    console.error('Failed to play audio:', error);
+                });
+            } else {
+                audio.pause();
+                button.innerHTML = '<img src="/images/icons/Play.svg" alt="">';
+            }
 
-          if (audio.paused) {
-              audio.play().then(() => {
-                  button.innerHTML = '<img src="images/icons/Pause.png" alt="">';
-              });
-          } else {
-              audio.pause();
-              button.innerHTML = '<img src="images/icons/Play.png" alt="">';
-          }
-
-          audio.addEventListener('timeupdate', () => {
-              var progress = (audio.currentTime / audio.duration) * 100;
-              progressBar.style.width = `${progress}%`;
-          });
-      });
-  });
-})()
+            audio.addEventListener('timeupdate', () => {
+                const progress = (audio.currentTime / audio.duration) * 100;
+                progressBar.style.width = `${progress}%`;
+            });
+        });
+    });
+});
