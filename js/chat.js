ef9a9223461dd1d43ed4a6791c74c281eb256c78
rivazhr <rifafairuzzahra@gmail.com>
2023-12-05 13:12:48 +0700

    ruangchat-fix2

--- js/chat.js
+++ js/chat.js
@@ -1,18 +1,27 @@
+document.getElementById('chatInput').addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+  }
+});
+
 document.getElementById('fileInput').addEventListener('change', function (event) {
   const fileInput = event.target;
-  const previewImage = document.getElementById('previewImage');
   const selectedFileDetails = document.getElementById('selectedFileDetails');
+  const name = document.getElementById('fileName');
+  const previewImage = document.getElementById('previewImage');
 
   if (fileInput.files.length > 0) {
     const file = fileInput.files[0];
     const fileName = file.name;
-    
-    selectedFileDetails.innerText = fileName;
-    if(selectedFileDetails != null){
+
+    name.textContent = fileName;
+    if(name.textContent != ""){
       selectedFileDetails.classList.remove('visually-hidden');
+    } else {
+      selectedFileDetails.classList.add('visually-hidden');
     }
 
-    if (/\.(jpeg|png|gif)$/i.test(fileName)) {
+    if (/\.(jpg|jpeg|png|gif)$/i.test(fileName)) {
       const reader = new FileReader();
       reader.onload = function (e) {
         previewImage.src = e.target.result;
@@ -22,41 +31,56 @@ document.getElementById('fileInput').addEventListener('change', function (event)
       previewImage.src = '';
     }
   } else {
-    selectedFileDetails.innerText = '';
+    selectedFileDetails.innerHTML = '';
     previewImage.src = '';
   }
 });
 
+function clearFile() {
+  const selectedFileDetails = document.getElementById('selectedFileDetails');
+  selectedFileDetails.classList.add('visually-hidden');
+
+  const previewImage = document.getElementById('previewImage');
+  previewImage.src = '';
+
+  const clearButton = document.getElementById('clear');
+  clearButton.style.display = 'none';
+}
+
+
 function send(fileInputId, chatInputId) {
+  const chatContainer = document.getElementById('ruangObrolan');
   const fileInput = document.getElementById(fileInputId);
   const chatInput = document.getElementById(chatInputId);
 
   const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : '';
   const messageText = chatInput.value;
 
-  fileInput.value = '';
-  chatInput.value = '';
-
-  const chatMessage = document.createElement('div');
-  chatMessage.classList.add('row', 'justify-content-end');
-  chatMessage.innerHTML = `
-    <div class="col-6 justify-content-end">
-      <div class="chatSelf p-2 px-3">
-        <div>
-          <p>${messageText}</p>
-          <p>${fileName ? `<strong>File:</strong> ${fileName}` : ''}</p>
+  if(messageText != "" || fileName != ""){
+    fileInput.value = '';
+    chatInput.value = '';
+  
+    const chatMessage = document.createElement('div');
+    chatMessage.classList.add('row', 'justify-content-end');
+    chatMessage.innerHTML = `
+      <div class="col-6 justify-content-end">
+        <div class="chatSelf p-2 px-3">
+          <div class="message-content">
+            <p>${messageText}</p>
+            <p>${fileName ? `<strong>File:</strong> ${fileName}` : ''}</p>
+          </div>
+          <div class="text-end timeSent" style="font-size: 10px;">${getCurrentTime()}</div>
         </div>
-        <div class="text-end timeSent" style="font-size: 10px;">${getCurrentTime()}</div>
       </div>
-    </div>
-  `;
-
-  const selectedFileDetails = document.getElementById('selectedFileDetails');
-  selectedFileDetails.innerText = ''; 
-  selectedFileDetails.classList.add('visually-hidden');
-
-  const chatContainer = document.getElementById('ruangObrolan');
-  chatContainer.appendChild(chatMessage);
+    `;
+  
+    const selectedFileDetails = document.getElementById('selectedFileDetails');
+    selectedFileDetails.classList.add('visually-hidden');
+  
+    chatContainer.appendChild(chatMessage);
+    
+  }
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
 function getCurrentTime() {
@@ -64,4 +88,14 @@ function getCurrentTime() {
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
+}
+
+function clearFile() {
+  const selectedFileDetails = document.getElementById('selectedFileDetails');
+  const form = document.getElementById('sendChat');
+    form.reset();
+  selectedFileDetails.classList.add('visually-hidden');
+  
+}
+function resetFileInput() {
 }
